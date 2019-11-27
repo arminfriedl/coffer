b3b86b42384e7575a5c3198694a6c77f4ced531e
Armin Friedl <dev@friedl.net>
2019-11-27 23:00:18 +0100

    [client] Set up environment, spawn subprocess
    - Coffer client sets up the environment from the keys and secrets read
    - Coffer client spawns a subprocess and reaps itself
    
    Signed-off-by: Armin Friedl <dev@friedl.net>

--- coffer-client/src/main.rs
+++ coffer-client/src/main.rs
@@ -7,7 +7,6 @@ use std::fs::File;
 use std::error::Error;
 use std::net::TcpStream;
 use std::net::SocketAddr;
-use std::io::Read;
 use std::net::IpAddr;
 use std::path::PathBuf;
 use std::io::BufRead;
@@ -16,51 +15,78 @@ use std::io::Write;
 
 #[derive(StructOpt, Debug)]
 struct Args {
-    /// Path to the keys file
-    #[structopt(short, long, parse(from_os_str), env = "SECSRV_SECRETS", hide_env_values = true)]
-    secrets: PathBuf,
-
     /// The port secsrv is listening on
     #[structopt(short, long, env = "SECSRV_PORT", default_value = "9187")]
     port: u16,
 
     /// The address secsrv binds to
     #[structopt(short, long, env = "SECSRV_IP", default_value = "127.0.0.1")]
-    ip: IpAddr
+    ip: IpAddr,
+
+    /// Path to the keys file
+    #[structopt(parse(from_os_str), env = "SECSRV_SECRETS", hide_env_values = true)]
+    secrets: PathBuf,
+
+    /// The subcommand spawned by coffer-client
+    cmd: String,
+
+    /// Arguments to the subcommand spawned by coffer-client
+    cmd_args: Vec<String>
 }
 
-type Secrets = Vec<String>;
-
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
     let args = Args::from_args();
 
-    info!{"Parsing sec requests"}
+    info!{"Connecting to coffer server"}
+    let addr = SocketAddr::from((args.ip, args.port));
+    let stream: TcpStream = TcpStream::connect(addr)?;
 
-    let secrets = parse_from_path(&args.secrets)?;
-
-    info!{"Connecting"}
-    let mut channel: TcpStream = TcpStream::connect(SocketAddr::from((args.ip, args.port)))?;
+    info!{"Parsing key requests"}
+    let keys = parse_from_path(&args.secrets)?;
 
     info!{"Reading secrets"}
+    retrieve_secrets(&keys, stream)?;
 
-    for s in secrets {
-        let buf = serde_cbor::to_vec(&s)?;
-        channel.write_all(&buf.len().to_be_bytes())?;
-        channel.write_all(&buf)?;
-        info!{"Wrote secret {} as {:?}", s, buf}
+    info!{"Spawning coffer'ed command, reaping coffer"}
+    reap_coffer(&args.cmd, &args.cmd_args);
 
+    Err("Could not spawn sub-command".into())
+}
+
+fn retrieve_secrets(keys: &Vec<String>, mut stream: TcpStream) -> Result<(), Box<dyn Error>>{
+    for k in keys {
+        let buf = serde_cbor::to_vec(&k)?;
+        info!{"Sending key request {} as {:?}", k, buf}
+        stream.write_all(&buf.len().to_be_bytes())?;
+        stream.write_all(&buf)?;
+
+        info!{"Reading response"}
+        let mut reader = BufReader::new(&stream); // get buffered reader for line-wise reading from stream
+
+        // read line
         let mut resp = String::new();
-        let mut reader = BufReader::new(&channel);
-        reader.read_line(&mut resp);
-        println!{"Resp: {:?}", resp};
+        reader.read_line(&mut resp)?;
+
+        info!{"Retrieved secret. Setting environment"}
+        std::env::set_var(k.trim(), resp.trim());
     }
 
     Ok(())
 }
 
-pub fn parse_from_path(path: &PathBuf) -> Result<Secrets, Box<dyn Error>> {
+fn reap_coffer(cmd: &str, args: &Vec<String>) {
+    let mut cmd = exec::Command::new(cmd);
+
+    // TODO Push cmd as first arg if not already set?
+    cmd.args(args);
+
+    let err = cmd.exec();
+    error!{"Could not execute sub-command {}", err};
+}
+
+fn parse_from_path(path: &PathBuf) -> Result<Vec<String>, Box<dyn Error>> {
     let sec_file = File::open(path)?;
 
-    Ok(serde_yaml::from_reader::<_,Secrets>(sec_file)?)
+    Ok(serde_yaml::from_reader::<_, Vec<String>>(sec_file)?)
 }
